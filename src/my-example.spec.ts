6224d4062538ce75b3d2f2f3956625d5c0392a27
Pascal Vos <vospascal@gmail.com>
2024-05-18 19:41:45 +0000

    fix prettier

--- src/my-example.spec.ts
+++ src/my-example.spec.ts
@@ -1,29 +1,26 @@
-import { describe, expect, it, vi } from "vitest";
-import { html } from "lit";
-import { nextTick, mountComponent } from "../test/index"
+import {describe, expect, it, vi} from 'vitest';
+import {html} from 'lit';
+import {nextTick, mountComponent} from '../test/index';
 
-describe("Button", () => {
-
-  it("renders inital count is 0", async () => {
+describe('Button', () => {
+  it('renders inital count is 0', async () => {
     const wrapper = await mountComponent(html`<my-element></my-element>`);
 
-    const button = wrapper.shadowRoot?.querySelector("button");
+    const button = wrapper.shadowRoot?.querySelector('button');
 
-    
-    expect(button).toBeDefined()
+    expect(button).toBeDefined();
 
-    expect(button!.textContent?.trim()).toBe("count is 0");
-
+    expect(button!.textContent?.trim()).toBe('count is 0');
   });
 
-  it("renders count is 2", async () => {
+  it('renders count is 2', async () => {
     const wrapper = await mountComponent(html`<my-element></my-element>`);
 
-    const button = wrapper.shadowRoot?.querySelector("button");
+    const button = wrapper.shadowRoot?.querySelector('button');
 
     const spy = vi.fn();
 
-    wrapper.addEventListener("click", spy);
+    wrapper.addEventListener('click', spy);
 
     button.click();
     button.click();
@@ -32,9 +29,6 @@ describe("Button", () => {
 
     expect(spy).toHaveBeenCalledTimes(2);
 
-
-    expect(button!.textContent?.trim()).toBe("count is 2");
-
+    expect(button!.textContent?.trim()).toBe('count is 2');
   });
-
 });
